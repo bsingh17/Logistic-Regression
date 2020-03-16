17c0fabfc3bfbdabd653af9c1de9951cc92a18c2
bsingh17 <61860973+bsingh17@users.noreply.github.com>
2020-03-16 08:14:31 +0530

    Add files via upload

---
+++ pass vs fail.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+dataset=pd.read_csv('pass or fail.csv')
+x=dataset.drop('Pass_Or_Fail',axis='columns')
+y=dataset.Pass_Or_Fail
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=0)
+
+from sklearn.linear_model import LogisticRegression
+reg=LogisticRegression()
+reg.fit(x_train,y_train)
+
+y_predict=reg.predict(x_test)
+
+print(reg.score(x_test,y_predict))
+
+from sklearn.metrics import confusion_matrix
+confusion=confusion_matrix(y_test,y_predict)
+print(confusion)
