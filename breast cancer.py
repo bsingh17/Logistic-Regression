d86da0f373b6168f3901b2e0b246320cbaf54fcb
bsingh17 <61860973+bsingh17@users.noreply.github.com>
2020-03-22 18:45:03 +0530

    Add files via upload

---
+++ breast cancer.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+dataset=pd.read_csv('breast_cancer_dataset.csv')
+
+x=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,-1].values
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.5,random_state=0)
+
+from sklearn.linear_model import LogisticRegression
+reg=LogisticRegression()
+reg.fit(x_train,y_train)
+y_predict=reg.predict(x_test)
+
+from sklearn.metrics import confusion_matrix
+confusion=confusion_matrix(y_test,y_predict)
+print(confusion)
+print(reg.score(x_test,y_test))
